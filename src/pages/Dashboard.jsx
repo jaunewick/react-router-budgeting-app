d9385c10a4c04b40aee81b98e726b6c7dd986ecc
Daniel Giao <daniel.giao@icloud.com>
2024-07-29 17:27:07 -0400

    Added hidden input to handle different forms

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,35 +5,64 @@ import { useLoaderData } from "react-router-dom"
 import { toast } from "react-toastify"
 
 // helper functions
-import { fetchData } from "../helpers"
+import { createBudget, fetchData } from "../helpers"
 
 // components
 import { Intro } from "../components/Intro"
+import { AddBudgetForm } from "../components/AddBudgetForm"
 
 // loader
 export function dashBoardLoader() {
     const userName = fetchData('userName')
-    return { userName }
+    const budgets = fetchData('budgets')
+    return { userName, budgets }
 }
 
 // action
 export async function dashboardAction({ request }) {
     const data = await request.formData()
-    const formData = Object.fromEntries(data)
-    try {
-        localStorage.setItem('userName', JSON.stringify(formData.userName))
-        return toast.success(`Welcome, ${formData.userName}!`)
-    } catch (e) {
-        throw new Error("There was a problem creating your account.")
+    const { _action, ...values } = Object.fromEntries(data)
+
+    if (_action === 'newUser') {
+        try {
+            localStorage.setItem('userName', JSON.stringify(values.userName))
+            return toast.success(`Welcome, ${values.userName}!`)
+        } catch (e) {
+            throw new Error("There was a problem creating your account.")
+        }
+    }
+
+    if (_action === "createBudget") {
+        try {
+            createBudget({
+                name: values.newBudget,
+                amount: values.newBudgetAmount
+            })
+            return toast.success("Budget created!")
+        } catch (error) {
+            throw new Error("There was a problem creating your budget.")
+        }
     }
 }
 
 export const Dashboard = () => {
-    const { userName } = useLoaderData()
+    const { userName, budgets } = useLoaderData()
     console.log(userName);
     return (
         <div>
-            {userName ? (<p>{userName}</p>) : <Intro />}
+            {userName ? (
+                <div className="dashboard">
+                    <h1>Welcome back, <span className="accent">{userName}</span></h1>
+                    <div className="grid-sm">
+                        {/* {budgets ? () : ()} */}
+                        <div className="grid-lg">
+                            <div className="flex-lg">
+                                <AddBudgetForm />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ) : <Intro />}
         </div>
     )
 }
